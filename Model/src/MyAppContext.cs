f3596205b9b68fc2687341d82360c447a604af31
Unknown <vladpromotorov@gmail.com>
2018-02-20 14:20:03 +0100

    Add MainViewModel

--- Model/src/MyAppContext.cs
+++ Model/src/MyAppContext.cs
@@ -1,6 +1,48 @@
-﻿namespace Model
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Model
 {
-    public class Class1
+    public class RssFeedItem
     {
+        public string Title { get; set; }
+        public string Excerpt { get; set; }
+
+        public RssFeedItem(string title, string excerpt)
+        {
+            Title = title;
+            Excerpt = excerpt;
+        }
+    }
+
+    public class RssFeedService
+    {
+        public async Task<List<RssFeedItem>> Load()
+        {
+            List<RssFeedItem> list = new List<RssFeedItem>();
+            WebClient client = new WebClient();
+            string data = client.DownloadString("https://lenta.ru/rss/news");
+            XmlReader reader = XmlReader.Create(new StringReader(data));
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            Console.WriteLine(feed.Title.Text);
+            Console.WriteLine("Items:");
+            foreach (SyndicationItem item in feed.Items)
+            {
+                /*Console.WriteLine("Title: {0}", item.Title.Text);
+                Console.WriteLine("Summary: {0}", (item.Summary).Text);*/
+                list.Add(new RssFeedItem($"{item.Title}", $"{item.Summary}"));
+            }
+            return list;
+        }
+    }
+    
+    public class MyAppContext
+    {
+        public static readonly RssFeedService FeedService = new RssFeedService();
     }
 }
---
+++ Shared/src/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Model;
+
+namespace App.Shared
+{
+    public class RssFeedViewModel : INotifyPropertyChanged
+    {
+        private List<RssFeedItem> _feed = new List<RssFeedItem>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RssFeedViewModel()
+        {
+        }
+
+        public async Task Load()
+        {
+            try
+            {
+                _feed = await MyAppContext.FeedService.Load();
+                FeedLoadingError = "";
+            }
+            catch (Exception e)
+            {
+                FeedLoadingError = e.Message;
+            }
+            finally
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Feed)));
+            }
+        }
+
+        public IReadOnlyList<RssFeedItem> Feed => _feed;
+        private string feedLoadingError;
+
+        public string FeedLoadingError
+        {
+            get
+            {
+                return feedLoadingError;
+            }
+
+            private set
+            {
+                feedLoadingError = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FeedLoadingError)));
+            }
+        }
+    }
+}
